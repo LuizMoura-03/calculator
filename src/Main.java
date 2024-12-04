04158d362300c9fc703083d6ce63e4540f1714ca
LuizFernando <barbosa.luiz@zup.com.br>
2024-12-04 14:13:40 -0300

    adding nextDouble method

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
 
+        System.out.println("Digite o um numero: ");
+        double numero1 = scanner.nextDouble();
+
+        System.out.println("Digite o um numero: ");
+        double numero2 = scanner.nextDouble()
+
 
     }
 
