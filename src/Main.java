20f749ecb7fed52bf6c317df584250c6f63c331f
LuizFernando <barbosa.luiz@zup.com.br>
2024-12-04 14:15:39 -0300

    correcting error

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
 
-        System.out.println("Digite o um numero: ");
+        System.out.println("Digite um numero: ");
         double numero1 = scanner.nextDouble();
 
-        System.out.println("Digite o um numero: ");
-        double numero2 = scanner.nextDouble()
+        System.out.println("Digite outro numero: ");
+        double numero2 = scanner.nextDouble();
 
 
     }
